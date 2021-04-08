903a7b1041b9edf72043729f64f47253ed4c3c4f
Ray Marceau <ray@raymarceau.com>
2021-04-08 15:08:58 -0700

    parse tracking id

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,8 @@
-alert('Hello World');
+const pageState = document.querySelctor(`[data-a-state='{"key":"page-state"}']`);
+const trackingId = "Not Defined"
+try {
+    JSON.parse(pageState.innerText).trackingId
+} catch (error) {
+    console.log("Unable to find trackingID", error)
+}
+alert("Tracking ID: " + trackingId)
