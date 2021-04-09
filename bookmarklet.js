c32d190de9f1f92e7abed2b7e660fe2edd2e47b4
Ray Marceau <ray@raymarceau.com>
2021-04-08 17:16:57 -0700

    try again text if no trackingID found

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,10 @@
 (function(){
 const pageState = document.querySelector(`[data-a-state='{"key":"page-state"}']`);
-let trackingId = "Not Defined";
 try {
-    trackingId = JSON.parse(pageState.innerText).trackingId;
+    const trackingId = JSON.parse(pageState.innerText).trackingId || "Package hasn't been shipped. Try again later";
+    alert("Tracking ID: " + trackingId);
 } catch (error) {
     console.log("PURSE | Unable to find trackingID: ", error);
+    alert("Error getting tracking ID.  You might want to alert support@purse.io.");
 }
-alert("Tracking ID: " + trackingId);
 })();
