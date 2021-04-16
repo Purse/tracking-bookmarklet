cbeb2267e752362315522e42759711d3f81abefe
Ray Marceau <ray@raymarceau.com>
2021-04-15 19:47:25 -0700

    tracking numbers sends to purse automatically

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,19 @@
-(function(){
-const pageState = document.querySelector(`[data-a-state='{"key":"page-state"}']`);
-try {
-    const trackingId = JSON.parse(pageState.innerText).trackingId || "Package hasn't been shipped. Try again later";
-    alert("Tracking ID: " + trackingId);
-} catch (error) {
-    console.log("PURSE | Unable to find trackingID: ", error);
-    alert("Error getting tracking ID.  You might want to alert support@purse.io.");
-}
+(function() {
+  try {
+    const pageState = document.querySelector('[data-a-state=\'{"key":"page-state"}\']');
+    const trackingId = JSON.parse(pageState.innerText).trackingId.trim();
+    const amazonOrderId = location.href.match(/orderId=(\d{3}-\d{7}-\d{7})/)[1];
+    if (!amazonOrderId) {
+      throw new Error('Cannot retrieve amazonOrderId from URL');
+    }
+    if (trackingId) {
+      alert("Click 'OK' to send Tracking Number to purse: " + trackingId);
+      location.href = 'https://purse.io/add-tracking/' + amazonOrderId + '/' + trackingId;
+    } else {
+      alert("It looks like the package hasn't been shipped yet.\nTry again later once it's been shipped");
+    }
+  } catch (error) {
+    console.log('Tracking-Bookmarklet Error:', error);
+    alert("Unable to retrieve Tracking Number.\nAre you sure you're on an Amazon tracking page?\nIf you think this is an error please send a message to support@purse.io to let us know.");
+  }
 })();
